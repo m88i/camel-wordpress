a286fa5d06f33a4b057b8ad73ba08c4a72927382
Ricardo Zanini <ricardozanini@gmail.com>
2018-01-02 19:27:07 -0200

    correcting unit tests after refactoring

--- src/test/java/org/m88i/camel/component/wordpress/WordpressPostOperationTest.java
+++ src/test/java/org/m88i/camel/component/wordpress/WordpressPostOperationTest.java
@@ -1,11 +1,7 @@
 package org.m88i.camel.component.wordpress;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.m88i.camel.component.wordpress.api.model.Post;
 import org.m88i.camel.component.wordpress.config.WordpressConfiguration;
@@ -14,29 +10,27 @@ import org.m88i.camel.component.wordpress.support.WordpressMockServerTestSupport
 public class WordpressPostOperationTest extends WordpressMockServerTestSupport {
 
     public WordpressPostOperationTest() {
-        
+
     }
-    
+
     @Test
     public void testPostSingleRequest() throws Exception {
         MockEndpoint mock = getMockEndpoint("mock:resultSingle");
-        mock.expectedMinimumMessageCount(1);       
+        mock.expectedMinimumMessageCount(1);
         mock.expectedBodyReceived().body(Post.class);
-        
-        assertMockEndpointsSatisfied(3, TimeUnit.SECONDS);
+
+        assertMockEndpointsSatisfied();
     }
-    
-    @Ignore
+
     @Test
     public void testPostListRequest() throws Exception {
         MockEndpoint mock = getMockEndpoint("mock:resultList");
-        mock.expectedMinimumMessageCount(1);       
-        mock.expectedBodyReceived().body(List.class);
-        
-        assertMockEndpointsSatisfied(90, TimeUnit.SECONDS);
+        mock.expectedMinimumMessageCount(1);
+        mock.expectedBodyReceived().body(Post.class);
+
+        assertMockEndpointsSatisfied();
     }
-    
-    
+
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
         return new RouteBuilder() {
@@ -47,12 +41,10 @@ public class WordpressPostOperationTest extends WordpressMockServerTestSupport {
                 configuration.setUrl(getServerBaseUrl());
                 component.setConfiguration(configuration);
                 getContext().addComponent("wordpress", component);
-                
-                from("wordpress:post?criteria.perPage=10")
-                    .to("mock:resultList");
-                
-                from("wordpress:post?id=114913")
-                  .to("mock:resultSingle");
+
+                from("wordpress:post?criteria.perPage=10").to("mock:resultList");
+
+                from("wordpress:post?id=114913").to("mock:resultSingle");
             }
         };
     }
